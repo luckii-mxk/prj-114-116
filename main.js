7b4acb95551969da7142d8e557fc8a47cab5f8c5
luckii-mxk <95324075+luckii-mxk@users.noreply.github.com>
2022-06-12 18:46:59 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,7 @@
+nose_x = 0;
+nose_y = 0;
+
+
 function preload() {
 
 }
@@ -5,12 +9,34 @@ function preload() {
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
+    video =createCapture(VIDEO);
+    video.size(300, 300);
+    video.hide;
+
+    poseNet = ml5.poseNet(video, modelLoaded);
+    poseNet.on("pose", gotPoses);
+}
+
+function modelLoaded() {
+    console.log("PoseNet Loaded Successfully :)")
 }
 
 function draw() {
-
+    image(video, 0, 0, 300, 300)
 }
 
 function take_photo() {
 
+}
+
+function gotPoses(results){
+    if(results.length > 0) {
+        console.log(results);
+
+        console.log("nose x = " + results[0].pose.nose.x);
+        console.log("nose y = " + results[0].pose.nose.y);
+
+        nose_x = results[0].pose.nose.x
+        nose_y = results[0].pose.nose.y
+    }
 }
