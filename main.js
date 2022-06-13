87abca00813c5c9eaee30b767e58f40475bf9221
luckii-mxk <95324075+luckii-mxk@users.noreply.github.com>
2022-06-12 19:14:07 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -9,7 +9,7 @@ function preload() {
 function setup() {
     canvas = createCanvas(300, 300);
     canvas.center();
-    video =createCapture(VIDEO);
+    video = createCapture(VIDEO);
     video.size(300, 300);
     video.hide;
 
@@ -23,10 +23,6 @@ function modelLoaded() {
 
 function draw() {
     image(video, 0, 0, 300, 300)
-}
-
-function take_photo() {
-
 }
 
 function gotPoses(results){
